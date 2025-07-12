5d3a33e4056553b81b353a21eff49779937ded5f
Dylan <dylantroche954@gmail.com>
2025-07-12 04:40:00 -0400

    final version, has all features implemented

--- main.py
+++ main.py
@@ -37,7 +37,6 @@ def extract_command(user_input: str, known_commands: set):
         if two_word_command in known_commands:
             return two_word_command, words[2:]
     if len(words) >= 3:
-        print("AHHHHHH")
         three_word_command = f"{words[0]} {words[1]} {words[2]}"
         if three_word_command in known_commands:
             return three_word_command, words[3:]
@@ -61,133 +60,156 @@ while True:
     user = user.lower()
     words = user.split()
     command, rest_words = extract_command(user, known_commands)
-    print(command)               
-    if user not in no_id_terms:
-        list_id = words[1]
-        listed_task = words[1:]
-        no_id = words[2:]
-        rejoined_sent = " ".join(listed_task)
-        rejoined_no_id = " ".join(no_id)
-    iteration += 1 #FIX THIS, iterates weirdly if add isnt run for a few steps
+    try:       
+        if user not in no_id_terms:
+            list_id = words[1]
+            listed_task = words[1:]
+            no_id = words[2:]
+            rejoined_sent = " ".join(listed_task)
+            rejoined_no_id = " ".join(no_id)
+    except IndexError: #Checking for blank inputs or errors
+        match command:
+            case "add":
+                print("Missing task name, please provide a task that you would like entered.")
+            case "delete" | "update":
+                print("Missing index value, please provide a number for what part of the list you would like to select.")
+        continue
+    
+    iteration += 1 
 
-    match command:
+    match command: #Master match that compares to user input
         case "add":
             task_time = check_time()
             task["name"] = rejoined_sent
-            task["id"] = iteration + 1 #FIX THIS, iterates weirdly
-            task["status"] = "Not Done"
+            task["id"] = iteration + 1 
+            task["status"] = "Not Done" #Assigning all values to add to list
             task["createdAt"] = task_time
             task_list.append(task.copy())
             print(f"Added Task! Task {task["id"]} has been set to '{task['name']}'.")
-            print(task)
         case "update":
-            task_time = check_time()
-            for item in task_list:
-                if item["id"] == int(list_id):
-                    item["name"] = rejoined_no_id
-                    task["updatedAt"] = task_time
-                    print(f"Updated Task! Task {item["id"]} has been set to '{item['name']}'.")
-                    iteration = reset_iteration(iteration,task_list)
-                    print(task)
-                    break
+            if not task_list:
+                print("No Tasks added, please add a task.")
+            else:
+                task_time = check_time()
+                for item in task_list:
+                    if item["id"] == int(list_id):
+                        item["name"] = rejoined_no_id #Adjusting Values to replace in list
+                        task["updatedAt"] = task_time
+                        print(f"Updated Task! Task {item["id"]} has been set to '{item['name']}'.")
+                        iteration = reset_iteration(iteration,task_list)
+                        break
         case "delete":
-            for item in task_list:
-                if item["id"] == int(list_id):
-                    task_list.remove(item)
-                    print(f"Deleted Task! Task {item["id"]}: '{item["name"]}' has been removed.")
-                    new_order = 1
-                    for item_sort in task_list:
-                        item_sort["id"] = new_order
-                        new_order += 1
-            iteration = reset_iteration(iteration,task_list)
+            if not task_list:
+                print("No Tasks added, please add a task.")
+            else:
+                for item in task_list:
+                    if item["id"] == int(list_id):
+                        task_list.remove(item)
+                        print(f"Deleted Task! Task {item["id"]}: '{item["name"]}' has been removed.") #Removing value from list and adjusting Id order
+                        new_order = 1
+                        for item_sort in task_list:
+                            item_sort["id"] = new_order
+                            new_order += 1
+                iteration = reset_iteration(iteration,task_list)
         case "mark todo":
-            two_char_id = words[2]
-            for item in task_list:
-                if item["id"] == int(two_char_id):
-                    item["status"] = "To Do"
-                    print(f"Updated Task! Task {item["id"]}: '{item["name"]}' has been set to To Do.")
-            iteration = reset_iteration(iteration,task_list)
+            if not task_list:
+                print("No Tasks added, please add a task.")
+            else:
+                try:
+                    two_char_id = words[2]
+                    for item in task_list:
+                        if item["id"] == int(two_char_id):
+                            item["status"] = "To Do" #Marking value as "To Do"
+                            print(f"Updated Task! Task {item["id"]}: '{item["name"]}' has been set to To Do.")
+                    iteration = reset_iteration(iteration,task_list)
+                except IndexError:
+                    print("Missing index value, please provide a number for what part of the list you would like to select.")
         case "mark done":
-            two_char_id = words[2]
-            for item in task_list:
-                if item["id"] == int(two_char_id):
-                    item["status"] = "Done"
-                    print(f"Updated Task! Task {item["id"]}: '{item["name"]}' has been set to Done.")
-            iteration = reset_iteration(iteration,task_list)
-        case "list todo":
-            print("list todoned")
-            is_occupied = False
-            for item in task_list:
-                if item["status"] == "To Do":
-                        print(f"{item["id"]}: {item["name"]}        Status: {item["status"]}")
-            if is_occupied == False:
-                print("No Tasks set to To Do.")
-            iteration = reset_iteration(iteration,task_list)
-        case "list in progress": 
-            is_occupied = False
-            for item in task_list:
-                if item["status"] == "In Progress":
-                        print(f"{item["id"]}: {item["name"]}        Status: {item["status"]}") 
-                        is_occupied = True
-            if is_occupied == False:
-                print("No Tasks set to In Progress.")
-            iteration = reset_iteration(iteration, task_list)
-        case "list done": 
-            is_occupied = False
-            for item in task_list:
-                if item["status"] == "Done":
-                        print(f"{item["id"]}: {item["name"]}        Status: {item["status"]}") 
-                        is_occupied = True
-            if is_occupied == False:
-                print("No Tasks set to Done.")
-            iteration = reset_iteration(iteration, task_list)
-        case "list":
-            print("listed")
-            is_occupied = False
-            for item in task_list:
-                print(f"{item["id"]}: {item["name"]}        Status: {item["status"]}")
-                is_occupied = True
-            if is_occupied == False:
-                print("No Tasks set.")
-            iteration = reset_iteration(iteration,task_list)
+            if not task_list:
+                print("No Tasks added, please add a task.")
+            else:
+                try:
+                    two_char_id = words[2]
+                    for item in task_list:
+                        if item["id"] == int(two_char_id): #Marking Value as "Done"
+                            item["status"] = "Done"
+                            print(f"Updated Task! Task {item["id"]}: '{item["name"]}' has been set to Done.")
+                    iteration = reset_iteration(iteration,task_list)
+                except IndexError:
+                    print("Missing index value, please provide a number for what part of the list you would like to select.")
         case "mark in progress":
+            if not task_list:
+                print("No Tasks added, please add a task.")
+            else:
+                try:
+                    is_occupied = False
+                    three_char_id = words[3]
+                    for item in task_list:
+                        if item["id"] == int(three_char_id): #Marking Value as "In Progress"
+                            item["status"] = "In Progress"
+                            is_occupied = True
+                            print(f"Updated Task! Task {item["id"]}: '{item["name"]}' has been set to In progress.") 
+                    iteration = reset_iteration(iteration,task_list)
+                except IndexError:
+                    print("Missing index value, please provide a number for what part of the list you would like to select.")
+        case "list todo":
+            if not task_list:
+                print("No Tasks added, please add a task.")
+            else:
+                is_occupied = False
+                for item in task_list:
+                    if item["status"] == "To Do": #Viewing All items with status "To Do"
+                            print(f"{item["id"]}: {item["name"]}        Status: {item["status"]}")
+                if is_occupied == False:
+                    print("No Tasks set to To Do.")
+                iteration = reset_iteration(iteration,task_list)
+        case "list in progress":
+            if not task_list:
+                print("No Tasks added, please add a task.")
+            else: 
+                is_occupied = False
+                for item in task_list:
+                    if item["status"] == "In Progress": #Viewing all items with status "In Progress"
+                            print(f"{item["id"]}: {item["name"]}        Status: {item["status"]}") 
+                            is_occupied = True
+                if is_occupied == False:
+                    print("No Tasks set to In Progress.")
+                iteration = reset_iteration(iteration, task_list)
+        case "list done": 
+            if not task_list:
+                print("No Tasks added, please add a task.")
+            else:
+                is_occupied = False
+                for item in task_list:
+                    if item["status"] == "Done": #Viewing all items with status "Done"
+                            print(f"{item["id"]}: {item["name"]}        Status: {item["status"]}") 
+                            is_occupied = True
+                if is_occupied == False:
+                    print("No Tasks set to Done.")
+                iteration = reset_iteration(iteration, task_list)
+        case "list":
             is_occupied = False
-            three_char_id = words[3]
             for item in task_list:
-                if item["id"] == int(three_char_id):
-                    item["status"] = "In Progress"
-                    is_occupied = True
-                    print(f"Updated Task! Task {item["id"]}: '{item["name"]}' has been set to In progress.") 
+                print(f"{item["id"]}: {item["name"]}        Status: {item["status"]}") #Viewing all items
+                is_occupied = True 
+            if is_occupied == False:
+                print("No Tasks added, please add a task.")
             iteration = reset_iteration(iteration,task_list)
         case "stop":
-            break
+            break #Stops script
         case "export":
-            filename = "export.json"
+            filename = "export.json" 
             try:
                 with open(filename, "w") as f:
-                    json.dump(task_list, f, indent=4)
-                print(f"JSON data succesfully written to {filename}")
+                    json.dump(task_list, f, indent=4) #Exports all values in list as json file
+                print(f"JSON data succesfully written to {filename}") 
             except IOError as e:
                 print(f"There was an error exporting to file: {e}")
         case _:
-            print("not valid!")
+            print("Not a valid command!")
             iteration = reset_iteration(iteration,task_list)
 
 
-
-
-
-#add
-#update
-#delete
-#mark task in progress
-#mark tasks that are done
-#list all tasks
-#list all tasks that are not done
-#list all tasks that are in progress
-#stop program
-
-#have a exception if the input conditions are not correct
 
 
 
